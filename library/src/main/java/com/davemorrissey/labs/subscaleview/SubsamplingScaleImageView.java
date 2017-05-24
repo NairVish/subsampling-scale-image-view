e86be0337088dce6b0ff711876dccf12594bcc78
Uula Ranta <uula.ranta@indooratlas.com>
2017-05-24 15:15:55 +0300

    Fix errors and separate getRotation api from superclass

--- library/src/main/java/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ library/src/main/java/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -730,7 +730,7 @@ public class SubsamplingScaleImageView extends View {
                         vTranslateStart.set(vTranslate.x, vTranslate.y);
                         vCenterStart.set((event.getX(0) + event.getX(1))/2, (event.getY(0) + event.getY(1))/2);
                         viewToSourceCoord(vCenterStart, sCenterStart);
-                    } else {
+                    } else if (!rotationEnabled) {
                         // Abort all gestures on second touch
                         maxTouchCount = 0;
                     }
@@ -1384,6 +1384,11 @@ public class SubsamplingScaleImageView extends View {
                 sourceToViewCoord(tile.sRect.left, tile.sRect.bottom),
         };
 
+        for (PointF pointF: corners) {
+            if (pointF == null) {
+                return false;
+            }
+        }
         final double rotation = this.rotation % (Math.PI * 2);
 
         if (rotation < Math.PI / 2) {
@@ -2573,9 +2578,16 @@ public class SubsamplingScaleImageView extends View {
     }
 
     /**
-     * Returns the current rotation value.
+     * Returns the current rotation value in degrees
      */
-    public final float getRotation() {
+    public final float getRotationDeg() {
+        return (float) Math.toDegrees(rotation);
+    }
+
+    /**
+     * Returns the current rotation value in radians.
+     */
+    public final float getRotationRad() {
         return rotation;
     }
 
